5e6ffd7193815978384df6889f72574fa6246546
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-02-04 12:17:19 +0530

    added image optimizer for vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {ViteImageOptimizer} from 'vite-plugin-image-optimizer'
 
 
 export default defineConfig({
